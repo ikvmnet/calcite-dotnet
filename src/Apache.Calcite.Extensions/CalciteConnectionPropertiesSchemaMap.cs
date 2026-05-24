a6bdf98cdbebe386a5e45bbbc8a626ee7a7f9b6e
Jerome Haltom <jhaltom@alethic.solutions>
2026-05-24 11:19:12 -0500

    d

--- src/Apache.Calcite.Extensions/CalciteConnectionPropertiesSchemaMap.cs
+++ src/Apache.Calcite.Extensions/CalciteConnectionPropertiesSchemaMap.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
 using System.Linq;
+using System.Linq.Expressions;
 
 using java.lang;
 using java.util;
